a2fc00f4495ca9c7e7037867e8ed205fc71fe8c8
dandayou <danil99danilov@gmail.com>
2025-05-05 16:29:26 +0300

    Create onepage website for "Ecoforum", make a structure and head of the page

---
+++ scripts.js
@@ -0,0 +1,40 @@
+class Carousel {
+    constructor() {
+        this.currentSlide = 0;
+        this.slides = document.querySelectorAll('.carousel-slide');
+        this.interval = null;
+        this.init();
+    }
+
+    init() {
+        // Инициализация первого слайда
+        this.updateSlides();
+        // Запуск автоматической смены слайдов
+        this.startAutoSlide();
+    }
+
+    updateSlides() {
+        this.slides.forEach(slide => {
+            slide.classList.remove('active');
+        });
+        this.slides[this.currentSlide].classList.add('active');
+    }
+
+    nextSlide() {
+        this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+        this.updateSlides();
+    }
+
+    startAutoSlide() {
+        this.interval = setInterval(() => this.nextSlide(), 5000);
+    }
+
+    stopAutoSlide() {
+        clearInterval(this.interval);
+    }
+}
+
+// Инициализация карусели после загрузки DOM
+document.addEventListener('DOMContentLoaded', () => {
+    new Carousel();
+}); 
